cbf49d625c3492bf4ffb5bdc2aef3a30ffa143de
magaramol09 <magaramol09@gmail.com>
2022-04-08 20:04:50 +0530

    Main-section-Completed

--- src/componets/API/MainAPI.js
+++ src/componets/API/MainAPI.js
@@ -4,19 +4,19 @@ const MainAPI = [
     id: 1,
     logo: "fas fa-download",
     title: "Downlaod App",
-    info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?",
+    info: "Lorem ipsum, dolor sit amet  consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?",
   },
   {
     id: 2,
     logo: "fas fa-chalkboard-teacher",
     title: "Complete the Instruction ",
-    info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?",
+    info: "Lorem ipsum, dolor sit amet  consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?",
   },
   {
     id: 3,
     logo: "fas fa-donate",
     title: "Receive your funds",
-    info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?",
+    info: "Lorem ipsum, dolor sit amet  consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?",
   },
   ];
   
