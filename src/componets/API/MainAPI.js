cea25d382fe8c5a9ffd691778fc98565f6654e47
magaramol09 <magaramol09@gmail.com>
2022-04-08 17:56:49 +0530

    Main-Section-Completed

--- src/componets/API/MainAPI.js
+++ src/componets/API/MainAPI.js
@@ -1,20 +1,23 @@
 
 const MainAPI = [
-    {
-      id: 1,
-      title: "Sign in",
-      info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, Etquia quis?Lorem ipsum,  Etquia quis?",
-    },
-    {
-      id: 2,
-      title: "Add your bank Account",
-      info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit.",
-    },
-    {
-      id: 3,
-      title: "Send payment request",
-      info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, consectetur adipisicing elit. Etquia quis?Lorem ipsum?",
-    },
+  {
+    id: 1,
+    logo: "fas fa-download",
+    title: "Downlaod App",
+    info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?",
+  },
+  {
+    id: 2,
+    logo: "fas fa-chalkboard-teacher",
+    title: "Complete the Instruction ",
+    info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?",
+  },
+  {
+    id: 3,
+    logo: "fas fa-donate",
+    title: "Receive your funds",
+    info: "Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?Lorem ipsum, dolor sit amet consectetur adipisicing elit. Etquia quis?",
+  },
   ];
   
   export default MainAPI;
--- src/componets/Main.jsx
+++ src/componets/Main.jsx
@@ -4,23 +4,21 @@ export default function Main() {
     const [data, setData] = useState(MainAPI)
     return (
         <>
-            <div className="work-container container">
+            <div className="main-container container">
                 <h1 className='main-heading text-center'>How does it work</h1>
                 <div className="row">
                     {data.map((curElem) => {
+                        const { id, logo, title, info } = curElem;
                         return <>
-                            <div className="col-12 col-lg-4 text-center">
-                                <i class="fa-solid fa-download"></i>
-                                <h2 className='sub-heading'>Download App</h2>
-                                <p className='main-hero-para w-100' >Lorem ipsum dolor sit amet consectetur adipisicing elit. Pariatur aliquid adipisci deserunt non aspernatur dignissimos maxime mollitia, eveniet, odio vel quod dolor suscipit provident beatae ex quam molestias voluptatibus? Dolorum.</p>
+                            <div className="col-12 col-lg-4 text-center main-container-subdiv">
+                                <i class={`fontawsome-style ${logo}`}></i>
+                                <h2 className='sub-heading'>{title}</h2>
+                                <p className='main-hero-para w-100'>{info}</p>
                             </div>
                         </>
                     })}
-
                 </div>
-
             </div>
-
         </>
     )
 }
