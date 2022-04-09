e4ffa6dc73ea3498d752dac3c8bfe6ad7c26d5a3
magaramol09 <magaramol09@gmail.com>
2022-04-09 16:24:17 +0530

    AboutUs-Second-Section-Completd

--- src/componets/About.jsx
+++ src/componets/About.jsx
@@ -18,26 +18,65 @@ export default function About() {
               </h3>
               <h1 className="main-heading ">How to use the App?</h1>
 
-              {data.map((curElem)=>{
-                const{id,title,info}=curElem
-                return(
+              {data.map((curElem) => {
+                const { id, title, info } = curElem
+                return (
                   <div className="row our-services-info" key={id}>
-                  <div className="col-1 our-services-number">{id}</div>
-                  <div className="col-10 our-services-list">
-                    <h2>{title}</h2>
-                    <p className='main-hero-para'>
-                     {info}
-                    </p>
-                    <br />
+                    <div className="col-1 our-services-number">{id}</div>
+                    <div className="col-10 our-services-list">
+                      <h2>{title}</h2>
+                      <p className='main-hero-para'>
+                        {info}
+                      </p>
+                      <br />
+                    </div>
                   </div>
-                </div>
                 )
               })}
-                  <button className=' services-btn'> Learn More</button>
+              <button className=' services-btn'> Learn More</button>
             </div>
           </div>
         </div>
       </section>
+
+      {/* About us Second Section  */}
+
+      <section className='common-section our-services aboutus-second-section'>
+        <div className="container mb-5">
+          <div className="row demo1">
+            {/* Right side colum  */}
+            <div className="col-12 col-lg-7 our-services-list">
+              <h3 className="mini-title">
+
+                -- SUPPORT IN ANY LANGUAGES
+              </h3>
+              <h1 className="main-heading "> World class support is <br /> available 24/7</h1>
+
+              {data.map((curElem) => {
+                const { id, title, info } = curElem
+                return (
+                  <div className="row our-services-info" key={id}>
+                    <div className="col-1 our-services-number">{id}</div>
+                    <div className="col-10 our-services-list">
+                      <h2>{title}</h2>
+                      <p className='main-hero-para'>
+                        {info}
+                      </p>
+                      <br />
+                    </div>
+                  </div>
+                )
+              })}
+              <button className=' services-btn'> Learn More</button>
+            </div>
+            <div className="col-12 col-lg-5 img-fluid our-service-leftside-img">
+              <img src="./images/callcenter.jpg" alt="aboutusIMg" />
+            </div>
+          </div>
+        </div>
+      </section>
+
+
     </>
   )
 }
